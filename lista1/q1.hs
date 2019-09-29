b88f5721c69b1f428cdf0df2011d5e24c9e6c901
gustavoisidio <gisf@cin.ufpe.br>
2019-09-29 17:31:30 -0300

    Q1: done

--- lista1/q1.hs
+++ lista1/q1.hs
@@ -1,3 +1,4 @@
+import Data.List
 ------- Codificacao
 
 let2int :: Char -> Int
@@ -9,6 +10,7 @@ int2let x = ( toEnum ( x + 97 ) )
 -- If the shift is greater than 25 there is more than 1 turn => Subtract 26 from the shift number
 -- If the shift is greater thant the distance to the alphaet's end => Use the remaining shift - 1 where the remaining shift is shift - distance to the end 
 shift :: Int -> Char -> Char
+shift sh ' ' = ' '
 shift sh x
     | sh > 25 = shift ( sh - 26 ) x
     | sh > disToTheEnd = int2let $ ( sh - disToTheEnd - 1 ) 
@@ -43,9 +45,23 @@ ex = map (\x -> 1) [1.0 .. 26.0]
 -- λ> chisqr (freqs "abbcccddddeeeee") (map fromInteger ex)
 
 rotate :: Int -> [ a ] -> [ a ]
+rotate 0 as = as
 rotate n as = right ++ left
     where left = take n as
           right = drop n as 
 
+crack :: String -> String
+crack a = encode ( 26 - rotationNumber ) a 
+    where table' = freqs a
+          tableChisqr = rotateChisqr 0 tableEnglish table'
+          rotationNumber = (\(Just x) -> x) (elemIndex ( minimum tableChisqr ) tableChisqr)    
+
+tableEnglish = [ 8.167, 1.492, 2.782, 4.253, 12.702, 2.228, 2.015, 6.094, 6.966, 0.153
+               , 0.772, 4.025, 2.406, 6.749, 7.507, 1.929, 0.095, 5.987, 6.327, 9.056
+               , 2.758, 0.978, 2.360, 0.150, 1.974, 0.074 ] :: [Float]
+
+rotateChisqr :: Int -> [Float] -> [Float] -> [Float]
+rotateChisqr 26 _ _ = []
+rotateChisqr n table table'= ( chisqr ( rotate n table' ) table ) : rotateChisqr ( n + 1 ) table table'
 
 
