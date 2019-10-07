096c59234ec13e651340166c0d17ffdd10b4d7f3
gustavoisidio <gisf@cin.ufpe.br>
2019-10-07 00:48:42 -0300

    erro corrigido, maos a obra

--- lista1/q4.hs
+++ lista1/q4.hs
@@ -12,43 +12,55 @@ empty = Set Nil
 sing :: a -> Set a
 sing x = Set ( Node x Nil Nil)
 
-instance Eq a => Eq ( Set a ) where
+instance Ord a => Eq ( Set a ) where
     (==) = eqSet
 
-class Eq a => Ord a where
-    eqSet :: Set a -> Set a -> Bool
-    eqSet ( Set xs ) ( Set ys ) = prepSet ( treeToList xs ) == prepSet ( treeToList ys )
-    
-    prepSet :: [ a ] -> [ a ]
-    prepSet x = map head . group . sort $ x
 
---instance Eq a => Eq (Tree a) where
-  --  Nil == Nil = True
-   -- Nil == _     = False
-   -- _ == Nil     = False
-   -- a1 == a2 = eqSet a1 a2 -- Incorreto
+instance Ord a => Eq (Tree a) where
+    Nil == Nil = True
+    Nil == _     = False
+    _ == Nil     = False
+    Node x xl xr == Node y yl yr = prepSet ( treeToList ( Node x xl xr ) ) == prepSet ( treeToList ( Node y yl yr ) )
 
---prepSet :: ( Ord a, Num a ) => [ a ] -> [ a ]
---prepSet x = map head . group . sort $ x
+-- Removes the duplicate elements
+prepSet :: Ord a => [ a ] -> [ a ]
+prepSet x = map head . group . sort $ x
 
+-- Convertes a Tree in to a list by in order algorithm
 treeToList :: Eq a => Tree a -> [ a ]
 treeToList ( Nil ) = [  ]
 treeToList ( Node x Nil Nil ) = [ x ]
 treeToList ( Node x ( Node y Nil Nil ) xr ) = y : x : treeToList xr
 treeToList ( Node x xl xr ) = treeToList xl ++ [ x ] ++ treeToList xr
 
---eqSet :: (Ord a, Num a) => Set a -> Set a -> Bool
---eqSet ( Set xs ) ( Set ys ) = prepSet ( treeToList xs ) == prepSet ( treeToList ys ) 
+-- Returns if a given set is equal to another set
+eqSet :: Ord a => Set a -> Set a -> Bool
+eqSet ( Set xs ) ( Set ys ) = xs == ys
 
-
+ex2 = ( Node 1 ( Node 2 ( Node 4 ( Node 1 Nil Nil ) Nil ) ( Node 5 Nil Nil ) ) ( Node 3 ( Node 6 Nil Nil ) ( Node 7 Nil Nil ) ) )
 
 ex1 = ( Node 1 ( Node 2 ( Node 4 Nil Nil ) ( Node 5 Nil Nil ) ) ( Node 3 ( Node 6 Nil Nil ) ( Node 7 Nil Nil ) ) )
 
+ex3 = ( Node 1 ( Node 2 ( Node 4 Nil Nil ) ( Node 5 Nil Nil ) ) ( Node 3 ( Node 6 Nil Nil ) Nil ) )
 
--- empty :: Set a
--- sing :: a -> Set a
--- memSet :: Ord a => Set a -> a -> Bool -- Verifica se o elemento faz parte da arvore
--- union :: Ord a => Set a -> Set a -> Set a -- Faz a uniao de duas arvores
+
+memSet :: Ord a => Set a -> a -> Bool -- Verifica se o elemento faz parte da arvore
+memSet ( Set ( Nil ) ) _ = False
+memSet ( Set ( Node x xl xr ) ) y
+    | x == y = True
+    | x < y = memSet ( Set xr ) y
+    | otherwise = memSet ( Set xl ) y
+
+
+union :: Ord a => Set a -> Set a -> Set a -- Faz a uniao de duas arvores
+union ( Set xs ) ( Set ys ) = Set ( uni xs ys )
+
+uni :: Ord a => Tree a -> Tree a -> Tree a
+--rootVal :: Ord a => Tree a -> a
+--rootVal 
+
+--{-
+---} 
 -- inter :: Ord a => Set a -> Set a -> Set a
 -- diff :: Ord a => Set a -> Set a -> Set a
 -- eqSet :: Eqa =>Seta->Seta->Bool
@@ -62,5 +74,3 @@ ex1 = ( Node 1 ( Node 2 ( Node 4 Nil Nil ) ( Node 5 Nil Nil ) ) ( Node 3 ( Node 
 -- flatten :: Set a -> [a]
    
 
---λ> map head . group . sort $ [1..3]
---[1,2,3]
--- lista1/tst.hs
+++ lista1/tst.hs
@@ -1,4 +1,33 @@
 import Data.List
 
+data Tree a = Nil
+            | Node a (Tree a) (Tree a)
+
+newtype Set a = Set ( Tree a )
+
+instance Eq a => Eq (Tree a) where
+    Nil == Nil = True
+    Nil == _     = False
+    _ == Nil     = False
+--    Node x xl xr == Node y yl yr = prepSet ( treeToList ( Node x xl xr ) ) == prepSet ( treeToList ( Node y yl yr ) )
+
+instance Eq a => Eq ( Set a ) where
+    (==) = eqSet
+
+-- Removes the duplicate elements 
 prepSet :: Ord a => [ a ] -> [ a ]
 prepSet x = map head . group . sort $ x
+
+-- Convertes a Tree in to a list by in order algorithm
+treeToList :: Eq a => Tree a -> [ a ]
+treeToList ( Nil ) = [  ]
+treeToList ( Node x Nil Nil ) = [ x ]
+treeToList ( Node x ( Node y Nil Nil ) xr ) = y : x : treeToList xr
+treeToList ( Node x xl xr ) = treeToList xl ++ [ x ] ++ treeToList xr
+
+-- Returns if a given set is equal to another set
+eqSet :: Eq a => Set a -> Set a -> Bool
+eqSet ( Set xs ) ( Set ys ) = xs == ys
+
+ex1 = ( Node 1 ( Node 2 ( Node 4 Nil Nil ) ( Node 5 Nil Nil ) ) ( Node 3 ( Node 6 Nil Nil ) ( Node 7 Nil Nil ) ) )
+
